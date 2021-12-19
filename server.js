08139d7b42953178a52d129c62b3670ae861827a
Khaled Elkhoreby <91973521+KhaledElkhoreby@users.noreply.github.com>
2021-12-19 21:20:15 +0200

    refactor:code

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
+// #region //TODO: Project Environment Setup
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log("The server is running");
+  console.log(`running on localhost:${port}`);
+});
+// #endregion
+
+// #region //TODO: APIs and Routes
+// TODO: APP API Endpoint
+projectData = {};
+
+// TODO: Return Endpoint Data
+// TODO: GET Route I: Server Side
+app.get("/test", (req, res) => {
+  res.send(projectData);
+  console.log("server sent data.");
+});
+
+//TODO: POST Route I : Server Side
+app.post("/all", (req, res) => {
+  const data = req.body;
+  projectData = data;
+  console.log("data was reserved successfully.");
+  console.log(projectData);
+});
+
+// #endregion
--- website/app.js
+++ website/app.js
@@ -1,5 +1,86 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+
+// TODO: Integrating OpenWeatherMap API
+const apiKey = "appid=5ff9367881db1860471c4f6e82e63475&units=imperial";
+let result = {};
+
+// TODO: Return Endpoint Data
+// TODO: GET Route II: Client Side
+const getData = async (url = "") => {
+  // if (!response.ok) {
+  // } else {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    result.temp = data.main.temp;
+  } catch (error) {
+    result = {};
+    result.date = newDate;
+    console.log("get error", error);
+    alert("Please enter valid zip code :(");
+  }
+  // }
+};
+
+//TODO: POST Route II : Client Side
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("post error", error);
+  }
+};
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = `${d.getMonth() + 1}.${d.getDate()}.${d.getFullYear()}`;
+result.date = newDate;
+// TODO: Naming HTML Inputs and Buttons For Interaction
+const zip = document.querySelector("#zip");
+const feelings = document.querySelector("#feelings");
+const generate = document.querySelector("#generate");
+let userInput = {};
+
+//TODO: Dynamic UI
+const retrieveData = async () => {
+  const request = await fetch("/test");
+  try {
+    // Transform into JSON
+    const allData = await request.json();
+    console.log(allData);
+    // Write updated data to DOM elements
+    document.getElementById("temp").innerHTML = `Temperature: ${
+      Math.round(allData.temp) || "___"
+    } degrees`;
+    document.getElementById("content").innerHTML = `Feeling: ${
+      allData.content ?? "no feeling"
+    }`;
+    document.getElementById("date").innerHTML = `Date: ${allData.date}`;
+  } catch (error) {
+    console.log("retrieve from server", error);
+  }
+};
+
+generate.addEventListener("click", async () => {
+  result.content = feelings.value;
+  let zipCode = zip.value;
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&${apiKey}`;
+  try {
+    await getData(baseURL);
+    postData("/all", result);
+    retrieveData();
+  } catch (error) {
+    result = {};
+    console.log(error);
+  }
+});
